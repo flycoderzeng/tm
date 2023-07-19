b87ef1e655507786ad8eb71b4af89dc3f01277bb
zengb <zengbo@olyminfosec.com>
2023-07-19 18:24:40 +0800

    修复bug

--- tm-web/src/main/java/com/tm/web/service/AutoPlanService.java
+++ tm-web/src/main/java/com/tm/web/service/AutoPlanService.java
@@ -1,18 +1,21 @@
 package com.tm.web.service;
 
-import com.tm.common.base.mapper.AutoPlanMapper;
-import com.tm.common.base.mapper.DataNodeMapper;
+import com.tm.common.base.mapper.*;
 import com.tm.common.base.model.AutoPlan;
 import com.tm.common.base.model.DataNode;
+import com.tm.common.base.model.PlanCase;
 import com.tm.common.base.model.User;
 import com.tm.common.entities.autotest.request.SaveNodeBody;
 import com.tm.common.entities.base.BaseResponse;
+import com.tm.common.entities.base.CommonTableQueryBody;
 import com.tm.common.entities.common.enumerate.DataTypeEnum;
 import com.tm.common.entities.common.enumerate.ResultCodeEnum;
 import com.tm.common.utils.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service("autoPlanService")
@@ -22,6 +25,12 @@ public class AutoPlanService extends BaseService {
     private AutoPlanMapper autoPlanMapper;
     @Autowired
     private DataNodeMapper dataNodeMapper;
+    @Autowired
+    private PlanCaseMapper planCaseMapper;
+    @Autowired
+    private PlanCaseSetupMapper planCaseSetupMapper;
+    @Autowired
+    private PlanCaseTeardownMapper planCaseTeardownMapper;
 
     public BaseResponse copy(SaveNodeBody body) {
         AutoPlan autoPlan = autoPlanMapper.selectByPrimaryId(body.getCopyId());
@@ -30,6 +39,38 @@ public class AutoPlanService extends BaseService {
         }
         autoPlan.setId(body.getId());
         autoPlanMapper.updateBySelective(autoPlan);
+
+        CommonTableQueryBody queryBody = new CommonTableQueryBody();
+        queryBody.setPlanId(body.getCopyId());
+        queryBody.setOffset(0);
+        queryBody.setPageSize(10000);
+        queryBody.setOrder("seq");
+        queryBody.setSort("asc");
+        List<PlanCase> planCases = planCaseMapper.queryList(queryBody);
+        if(planCases != null && !planCases.isEmpty()) {
+            for (PlanCase planCase : planCases) {
+                planCase.setPlanId(autoPlan.getId());
+                planCase.setId(null);
+            }
+            planCaseMapper.batchInsert(planCases);
+        }
+        List<PlanCase> planCases1 = planCaseSetupMapper.queryList(queryBody);
+        if(planCases1 != null && !planCases1.isEmpty()) {
+            for (PlanCase planCase : planCases1) {
+                planCase.setPlanId(autoPlan.getId());
+                planCase.setId(null);
+            }
+            planCaseSetupMapper.batchInsert(planCases1);
+        }
+        List<PlanCase> planCases2 = planCaseTeardownMapper.queryList(queryBody);
+        if(planCases2 != null && !planCases2.isEmpty()) {
+            for (PlanCase planCase : planCases2) {
+                planCase.setPlanId(autoPlan.getId());
+                planCase.setId(null);
+            }
+            planCaseTeardownMapper.batchInsert(planCases2);
+        }
+
         return ResultUtils.success();
     }
 
